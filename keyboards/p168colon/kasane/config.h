9f1698b72a9dfc83041a379bcd5c34e52d8307ad
hachi868 <dev@hachi868.com>
2022-12-25 22:32:51 +0900

    [update]adjustment based on deprecated

--- keyboards/p168colon/kasane/config.h
+++ keyboards/p168colon/kasane/config.h
@@ -2,9 +2,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
-#define VENDOR_ID    0x1FE8
-#define PRODUCT_ID   0x41A1
-#define DEVICE_VER   0x0001
-#define MANUFACTURER P168:
-#define PRODUCT kasane
 
